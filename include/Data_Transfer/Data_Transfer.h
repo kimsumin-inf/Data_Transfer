e64aed0e602d5edd152ec662e4f41a18a7c87e70
sumin <ksm4465ksm@chungbuk.ac.kr>
2022-08-18 13:44:58 +0900

    commit update

--- include/Data_Transfer/Data_Transfer.h
+++ include/Data_Transfer/Data_Transfer.h
@@ -1,6 +1,12 @@
 #pragma Once
 #include <ros/ros.h>
+
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
@@ -9,23 +15,32 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 #include <darknet_ros_msgs/BoundingBoxes.h>
 #include <darknet_ros_msgs/ObjectCount.h>
-#include "data_transfer_msg/data_transfer.h"
+#include <data_transfer_msg/data_transfer.h>
+#include <sensor_msgs/Image.h>
 
 class Data{
 private:
+    struct id_bbox{
+        int id;
+        int center;
+    };
+
     void obj_cnt_CB(const darknet_ros_msgs::ObjectCount::ConstPtr& msg);
     void obj_bbox_CB(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg);
+    void cam_CB(const sensor_msgs::Image::ConstPtr &msg);
     void Traffic_light(int id);
     void Delivery(int id);
-
+    static bool compare(id_bbox a, id_bbox b);
 
     ros::NodeHandle nh;
 
     ros::Subscriber subCOUNT;
     ros::Subscriber subBBOX;
+    ros::Subscriber subCAM;
 
     ros::Publisher pubRESULT;
 
@@ -34,6 +49,7 @@ private:
 
     int traffic_count;
     int delivery_count;
+    bool delivery_Init;
 
     std::string now_traffic_light_state;
     std::string init_delivery_state;
@@ -41,10 +57,13 @@ private:
 
     darknet_ros_msgs::ObjectCount cnt;
     darknet_ros_msgs::BoundingBoxes bbox;
+    cv_bridge::CvImagePtr cv_ptr;
+    cv::Mat base_frame;
 
     Eigen::MatrixXd traffic_light;
     Eigen::MatrixXd delivery;
-    Data_Transfer::data_transfer result;
+    data_transfer_msg::data_transfer result;
+    std::vector<id_bbox> vec;
 
 
 
--- src/Data_Transfer.cpp
+++ src/Data_Transfer.cpp
@@ -1,21 +1,32 @@
 #include "Data_Transfer/Data_Transfer.h"
+
 using namespace std;
+using namespace cv;
+
+/*
+ * funcition initialize;
+ */
 static inline void updateSample(Eigen::MatrixXd &sample, Eigen::MatrixXd &data);
 int mode(Eigen::MatrixXd data, int size);
 string return_ID(int value);
+string to_go(int value);
+void return_grayscale (Mat &frame);
+int return_center_point(int x_min, int x_max);
+
 
 Data::Data()
 : nh("")
 {
     subCOUNT = nh.subscribe("/darknet_ros/found_object" ,1,&Data::obj_cnt_CB, this);
     subBBOX = nh.subscribe("/darknet_ros/bounding_boxes" ,1,&Data::obj_bbox_CB, this);
-
-    pubRESULT = nh.advertise<Data_Transfer::data_transfer>("/Data_Transfer",1);
+    subCAM = nh.subscribe("/camera_front/image_raw", 1, &Data::cam_CB, this);
+    pubRESULT = nh.advertise<data_transfer_msg::data_transfer>("/Data_Transfer",1);
 
     count =0;
     size = 15;
     traffic_count=0;
     delivery_count=0;
+    delivery_Init = true;
     traffic_light = Eigen::MatrixXd::Zero(size,1);
     delivery = Eigen::MatrixXd::Zero(size,1);
 
@@ -31,18 +42,37 @@ void Data::obj_cnt_CB(const darknet_ros_msgs::ObjectCount::ConstPtr &msg) {
 
 void Data::obj_bbox_CB(const darknet_ros_msgs::BoundingBoxes::ConstPtr &msg) {
     bbox = *msg;
-    for (auto i  : bbox.bounding_boxes){
 
-        if (i.id < 5){
+    vec.clear();
+
+    for (auto i : bbox.bounding_boxes){
+        id_bbox tmp;
+        tmp.id = i.id;
+        tmp.center = return_center_point(i.xmin, i.xmax);
+        vec.push_back(tmp);
+    }
+    sort(vec.begin(),vec.end(), compare);
+    for (auto i : vec){
+        if (i.id <5){
             Traffic_light(i.id);
         }
-        else {
-            Delivery(i.id);
+    }
+    try{
+        for (auto i : vec){
+            if (i.id >=5){
+                Delivery(i.id);
+                break;
+            }
         }
-
+    }
+    catch (int expn){
+        cout << expn <<endl;
     }
 
+
 }
+
+
 
 void Data::Traffic_light(int id)  {
     Eigen::MatrixXd data = Eigen::MatrixXd::Zero(1,1);
@@ -53,25 +83,65 @@ void Data::Traffic_light(int id)  {
     if (traffic_count >size){
         now_traffic_light_state= return_ID(mode(traffic_light,size));
         result.traffic_light = now_traffic_light_state;
+
         if ((now_traffic_light_state=="R" || now_traffic_light_state=="LR" || now_traffic_light_state =="Y")){
             result.stop_line_ignore = false;
         }
         else {
             result.stop_line_ignore = true;
         }
+
         pubRESULT.publish(result);
     }
     else{
-        cout <<"waiting"<<endl;
+        ROS_INFO("Waiting for Traffic");
+
     }
     traffic_count+=1;
 
 
 }
-
 void Data::Delivery(int id) {
+    Eigen::MatrixXd data = Eigen::MatrixXd::Zero(1,1);
+    data<< id;
+    updateSample(delivery, data);
+    if (delivery_count >size){
+        now_delivery_state = return_ID(mode(delivery,size));
+        result.detect_Delivery = now_delivery_state;
+        if (delivery_Init == true){
+            init_delivery_state = return_ID(mode(delivery,size));
+            result.init_Delivery = init_delivery_state;
+            result.definition_Delivery = to_go(id);
+            delivery_Init = false;
+        }
+        pubRESULT.publish(result);
 
+    }
+    else{
+        ROS_INFO("Waiting for Delivery");
+
+    }
+    delivery_count+=1;
 }
+void Data::cam_CB(const sensor_msgs::Image::ConstPtr &msg) {
+    ROS_INFO("cam subscirbed : (%d , %d)", msg->width, msg->height);
+
+    try{
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
+        base_frame = cv_ptr->image;
+    } catch(cv_bridge::Exception &e){
+        ROS_ERROR("Error to Convert");
+        return;
+    }
+    return_grayscale(base_frame);
+    imshow("front_cam",base_frame);
+    waitKey(1);
+}
+void return_grayscale (Mat& frame){
+    cvtColor(frame,frame,COLOR_BGR2GRAY);
+}
+
+
 static inline void updateSample(Eigen::MatrixXd &sample, Eigen::MatrixXd &data)
 {
     if (data.cols() != sample.cols() || data.rows() != 1)
@@ -83,16 +153,17 @@ static inline void updateSample(Eigen::MatrixXd &sample, Eigen::MatrixXd &data)
     sample.block(0,0,sample.rows() - 1, sample.cols()) = temp;
     sample.block(sample.rows()- 1, 0, 1, sample.cols()) = data;
 }
-int mode(Eigen::MatrixXd data, int size){
-    int class_size =10;
-    vector <int> tmp(class_size,0);
-    for (int i=0;i<size;i++){
-        tmp[data(i,0)] +=1;
+
+int mode(Eigen::MatrixXd data, int size) {
+    int class_size = 10;
+    vector<int> tmp(class_size, 0);
+    for (int i = 0; i < size; i++) {
+        tmp[data(i, 0)] += 1;
     }
-    int max=tmp[0];
+    int max = tmp[0];
     int max_index = 0;
-    for (int i =0; i<class_size;i++){
-        if(max<tmp[i]){
+    for (int i = 0; i < class_size; i++) {
+        if (max < tmp[i]) {
             max = tmp[i];
             max_index = i;
         }
@@ -100,6 +171,7 @@ int mode(Eigen::MatrixXd data, int size){
 
     return max_index;
 }
+
 string return_ID(int value){
     switch(value){
         case 0:
@@ -125,4 +197,26 @@ string return_ID(int value){
         case 10:
             return "B3";
     }
+}
+string to_go(int value){
+    switch (value){
+        case 5:
+            return "B1";
+        case 6:
+            return "B2";
+        case 7:
+            return "B3";
+        case 8:
+            return "A1";
+        case 9:
+            return "A2";
+        case 10:
+            return "A3";
+    }
+}
+int return_center_point(int x_min, int x_max){
+    return (x_min + x_max) /2;
+}
+bool Data::compare(id_bbox a, id_bbox b){
+    return a.center > b.center;
 }
